0cb5492c932874ecbc3b7814d1aad097a3183bf5
Eliya Tal <88724245+ELIYAYACOB@users.noreply.github.com>
2022-07-08 18:49:23 +0300

    Add files via upload

---
+++ js/html_actuator.js
@@ -0,0 +1,195 @@
+function HTMLActuator() {
+  this.tileContainer    = document.querySelector(".tile-container");
+  this.scoreContainer   = document.querySelector(".score-container");
+  this.bestContainer    = document.querySelector(".best-container");
+  this.messageContainer = document.querySelector(".game-message");
+  this.sharingContainer = document.querySelector(".score-sharing");
+
+  this.score = 0;
+}
+
+HTMLActuator.prototype.actuate = function (grid, metadata) {
+  var self = this;
+
+  window.requestAnimationFrame(function () {
+    self.clearContainer(self.tileContainer);
+
+    grid.cells.forEach(function (column) {
+      column.forEach(function (cell) {
+        if (cell) {
+          self.addTile(cell);
+        }
+      });
+    });
+
+    self.updateScore(metadata.score);
+    self.updateBestScore(metadata.bestScore);
+
+    if (metadata.terminated) {
+      if (metadata.over) {
+        self.message(false); // You lose
+      } else if (metadata.won) {
+        self.message(true); // You win!
+      }
+    }
+
+  });
+};
+
+// Continues the game (both restart and keep playing)
+HTMLActuator.prototype.continue = function () {
+  if (typeof ga !== "undefined") {
+    ga("send", "event", "game", "restart");
+  }
+
+  this.clearMessage();
+};
+
+HTMLActuator.prototype.clearContainer = function (container) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+};
+
+HTMLActuator.prototype.addTile = function (tile) {
+  var text=new Array(18);
+  text[0] = " ";
+  text[1] = "2";
+  text[2] = "4";
+  text[3] = "8";
+  text[4] = "16";
+  text[5] = "32";
+  text[6] = "64";
+  text[7] = "128";
+  text[8] = "256";
+  text[9] = "512";
+  text[10] = "1,024";
+  text[11] = "2,048";
+  text[12] = "4,096";
+  text[13] = "8,192";
+  text[14] = "16,384";
+  text[15] = "32,768";
+  text[16] = "65,536";
+  text[17] = "131,072";
+  
+  var self = this;
+  var text2 = function (n) { var r = 0; while (n > 1) r++, n >>= 1; return r; }
+
+  var wrapper   = document.createElement("div");
+  var inner     = document.createElement("div");
+  var position  = tile.previousPosition || { x: tile.x, y: tile.y };
+  var positionClass = this.positionClass(position);
+
+  // We can't use classlist because it somehow glitches when replacing classes
+  var classes = ["tile", "tile-" + tile.value, positionClass];
+
+  if (tile.value > 131072) classes.push("tile-super");
+
+  this.applyClasses(wrapper, classes);
+
+  inner.classList.add("tile-inner");
+  inner.innerHTML = text[text2(tile.value)];
+
+  if (tile.previousPosition) {
+    // Make sure that the tile gets rendered in the previous position first
+    window.requestAnimationFrame(function () {
+      classes[2] = self.positionClass({ x: tile.x, y: tile.y });
+      self.applyClasses(wrapper, classes); // Update the position
+    });
+  } else if (tile.mergedFrom) {
+    classes.push("tile-merged");
+    this.applyClasses(wrapper, classes);
+
+    // Render the tiles that merged
+    tile.mergedFrom.forEach(function (merged) {
+      self.addTile(merged);
+    });
+  } else {
+    classes.push("tile-new");
+    this.applyClasses(wrapper, classes);
+  }
+
+  // Add the inner part of the tile to the wrapper
+  wrapper.appendChild(inner);
+
+  // Put the tile on the board
+  this.tileContainer.appendChild(wrapper);
+};
+
+HTMLActuator.prototype.applyClasses = function (element, classes) {
+  element.setAttribute("class", classes.join(" "));
+};
+
+HTMLActuator.prototype.normalizePosition = function (position) {
+  return { x: position.x + 1, y: position.y + 1 };
+};
+
+HTMLActuator.prototype.positionClass = function (position) {
+  position = this.normalizePosition(position);
+  return "tile-position-" + position.x + "-" + position.y;
+};
+
+HTMLActuator.prototype.updateScore = function (score) {
+  this.clearContainer(this.scoreContainer);
+
+  var difference = score - this.score;
+  this.score = score;
+
+  this.scoreContainer.textContent = this.score;
+
+  if (difference > 0) {
+    var addition = document.createElement("div");
+    addition.classList.add("score-addition");
+    addition.textContent = "+" + difference;
+
+    this.scoreContainer.appendChild(addition);
+  }
+};
+
+HTMLActuator.prototype.updateBestScore = function (bestScore) {
+  this.bestContainer.textContent = bestScore;
+};
+
+HTMLActuator.prototype.message = function (won) {
+  var mytxt=new Array(18);
+  
+  mytxt[0]="我就不信有人能死在这里！";
+  mytxt[1]="这一步也不可能有人死！";
+  mytxt[2]="洪丝丝不给你巧克力不帮你牵红线！";
+  mytxt[3]="程若晖在帮别人叠心心哦！~\(≧▽≦)/~";
+  mytxt[4]="no zuo no die";
+  mytxt[5]="芳芳的美腿儿离你越来越远╭(╯^╰)╮";
+  mytxt[6]="Wing~你还没有win哦(*^__^*)";
+  mytxt[7]="芳儿芳儿快马加鞭再来一局";
+  mytxt[8]="张美真爱钱钱不爱你！~~~~(>_<)~~~~ ";
+  mytxt[9]="成！天！懒！打游戏不能偷懒呢";
+  mytxt[10]="老婆大人判你重新开始";
+  mytxt[11]="椰菜头姐姐不要走";
+  mytxt[12]="你忍心看着江玉麟女儿身被发现而不去救她？";
+  mytxt[13]="丢颗仙豆，力量加满fighting~";
+  mytxt[14]="从来就只有一个燕红叶！";
+  mytxt[15]="我希望你是永远拥有我家钥匙的吕楞";
+  mytxt[16]="玩到这一步了还不截个图发微博吗？";
+  mytxt[17]="嘿，你把游戏打爆了...";
+ 
+  
+  var text3 = function (m) { var r = 0; while (m > 1) r++, m >>= 1; return r; }
+  var type    = won ? "game-won" : "game-over";
+  var message = won ? "宣萱对你说：你是最棒的！" : mytxt[text3(maxscore)-1];
+
+  if (typeof ga !== "undefined") {
+    ga("send", "event", "game", "end", type, this.score);
+  }
+
+  this.messageContainer.classList.add(type);
+  this.messageContainer.getElementsByTagName("p")[0].textContent = message;
+
+  this.clearContainer(this.sharingContainer);
+};
+
+HTMLActuator.prototype.clearMessage = function () {
+  // IE only takes one value to remove at a time.
+  this.messageContainer.classList.remove("game-won");
+  this.messageContainer.classList.remove("game-over");
+};
+
